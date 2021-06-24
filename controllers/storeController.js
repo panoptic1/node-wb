b591153c11dfec34aed37ce3c133d32a2a9293d1
Ryan Helgerson <ryan.helgerson@gmail.com>
2021-06-24 16:27:07 -0500

    Working on editStore route

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -15,3 +15,18 @@ exports.createStore = async (req, res) => {
     req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
     res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async(req, res) => {
+    // 1. Query the database for a list of all stores
+    const stores = await Store.find();
+    console.log(stores);
+    //parameter 1 refers to the 'stores' template, parameter 2 indicates what should be rendered on the page?
+    res.render('stores', { title: 'Stores', stores});
+}
+
+exports.editStore = async (req, res) => {
+    //1. Find the store given the ID
+    res.json(req.params);
+    //2. Confirm that they are the owner of the store
+    //3. Render the edit form so that the user can update their store
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 //composition: Wrapping a function within an error-handling function in order to avoid cumbersome try/catch statements
 const { catchErrors } = require('../handlers/errorHandlers');
-router.get('/', storeController.homePage);
+// router.get('/', storeController.homePage);
+//now that we can query the database, this^^ line of code becomes...
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
 router.post('/add', catchErrors(storeController.createStore));
+router.get('stores/:id/edit', catchErrors(storeController.editStore));
 module.exports = router;
